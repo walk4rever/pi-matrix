1ae565ed4593329c7c66448ea04768800f00bfdc
Rafael129 <walkklaw@gmail.com>
2026-04-20 23:25:09 +0800

    chore: update from email to matrix@air7.fun

--- cloud/api/app/config.py
+++ cloud/api/app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     gateway_key: str = ""
     orchestrator_url: str = "http://orchestrator:8000"
     resend_api_key: str = ""
-    from_email: str = "pi-matrix <hi@air7.fun>"
+    from_email: str = "pi-matrix <matrix@air7.fun>"
     dashboard_url: str = "https://matrix.air7.fun"
 
     class Config:
